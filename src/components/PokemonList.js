8ed40bd1113709bbd0962897aeed5c112fd77d61
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-08-27 10:37:35 -0300

    Update PokemonList.js

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,7 +2,7 @@ function PokemonList({className, pokemon, setInput,  SearchPokemon}) {
     function Suggestion(pokemon) {
         setInput(pokemon)
         
-        SearchPokemon()
+        SearchPokemon(e)
     }
   
     return <p onClick={()=> Suggestion(pokemon)} className={className}>{pokemon}</p>
