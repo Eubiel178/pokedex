520397ffec5cb258bcf9524feb11e3f8b3125fd3
Eubiel178 <98427107+Eubiel178@users.noreply.github.com>
2022-08-31 11:41:25 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,101 +1,106 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-import api from './services/api';
-import PokemonList from './components/PokemonList';
+import api from "./services/api";
+import PokemonList from "./components/PokemonList";
 
 function App() {
-  const[input, setInput] = useState('')
-  const[pokemonInfo, setPokemonInfo] = useState({})
-  const[image, setImage] = useState('')
-  const[notFound, setNotFound] = useState("Search...")
-  const[filterSearch, setFilterSearch] = useState([])
-  const[pokemons, setPokemons] = useState()
-  
-  async function HandleSearch(e) {
-    setInput(e.target.value)
-    
-    const response = await api.get()
-    setFilterSearch(response.data.results)
+  const [input, setInput] = useState("");
+  const [pokemonInfo, setPokemonInfo] = useState({});
+  const [image, setImage] = useState("");
+  const [notFound, setNotFound] = useState("Search...");
+  const [filterSearch, setFilterSearch] = useState([]);
+  const [pokemons, setPokemons] = useState();
+
+  async function HandleSearch(event) {
+    setInput(event.target.value);
+
+    const response = await api.get();
+    setFilterSearch(response.data.results);
 
     let pokemonsFiltred = filterSearch.filter((pokemon) => {
-      return pokemon.name.includes(input.toLowerCase())
-    })
+      return pokemon.name.includes(input.toLowerCase());
+    });
 
-    if(input.length > 1 && pokemonsFiltred.length > 0) {
-      setPokemons(pokemonsFiltred)
+    if (input.length > 1 && pokemonsFiltred.length > 0) {
+      setPokemons(pokemonsFiltred);
     } else {
-      setPokemons('')
+      setPokemons("");
     }
   }
 
   async function SearchPokemon(event) {
-    event.preventDefault()
-    const pokemon = input.toLowerCase()
-    setInput("")
-    
-    try {
-      const response = await api.get(`/${pokemon}`)
-    
-      setPokemonInfo(response)
-      setImage(response.data.sprites.versions['generation-v']['black-white']['animated']['front_default'])
-    } catch (error) {
-      setImage('')
-      setNotFound('Pokemon not found...')
-    }
-    }
-  
+    event.preventDefault();
 
-  return(
+    const pokemon = input.toLowerCase();
+    setInput("");
+
+    try {
+      const response = await api.get(`/${pokemon}`);
+
+      setPokemonInfo(response);
+      setImage(
+        response.data.sprites.versions["generation-v"]["black-white"][
+          "animated"
+        ]["front_default"]
+      );
+    } catch (error) {
+      setImage("");
+      setNotFound("Pokemon not found...");
+    }
+  }
+
+  return (
     <main>
-      <div className='AppContainer'>
-        {image == '' &&(
+      <div className="AppContainer">
+        {image == "" && (
           <section>
-            <img className='Image' src='./image.jpeg'/>
-            <h3 className='PokemonName'>{notFound}</h3>
+            <img className="Image" src="./image.jpeg" />
+            <h3 className="PokemonName">{notFound}</h3>
           </section>
         )}
 
-        {image != ''  &&(
+        {image != "" && (
           <section>
-            <div className='PokemonInfoContainer'>
-              <img className='Gif' src={image}/>
+            <div className="PokemonInfoContainer">
+              <img className="Gif" src={image} />
             </div>
 
-            <h3 className='PokemonName'>{pokemonInfo.data.name.toUpperCase()} - {pokemonInfo.data.id}</h3>
+            <h3 className="PokemonName">
+              {pokemonInfo.data.name.toUpperCase()} - {pokemonInfo.data.id}
+            </h3>
           </section>
-          )}
+        )}
 
-          <div className='DivContainer'>
-            <form onSubmit={SearchPokemon} className='Form'>
-              <input
-                className='Input' 
-                onChange={HandleSearch} 
-                type='search' 
-                value = {input} 
-                placeholder='id or name'  
-                required
-              />
+        <div className="DivContainer">
+          <form onSubmit={SearchPokemon} className="Form">
+            <input
+              className="Input"
+              onChange={HandleSearch}
+              type="search"
+              value={input}
+              placeholder="id or name"
+              required
+            />
+          </form>
+
+          {pokemons && (
+            <form className="SuggestionsList">
+              {pokemons.map((element, index) => (
+                <PokemonList
+                  id={index}
+                  pokemon={element.name}
+                  setInput={setInput}
+                  setPokemons={setPokemons}
+                  SearchPokemon={SearchPokemon}
+                />
+              ))}
             </form>
-
-            {pokemons &&(
-              <div className='SuggestionsList'>
-                {pokemons.map((element, index) => (
-                  <PokemonList 
-                    className='SuggestionsItem' 
-                    id={index}
-                    pokemon={element.name}
-                    setInput={setInput}
-                    setPokemons={setPokemons}
-                    search={SearchPokemon}
-                  />
-                ))}
-              </div>
-            )}
-          </div>
+          )}
+        </div>
       </div>
     </main>
-  )
+  );
 }
 
 export default App;
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,10 +1,26 @@
-function PokemonList({className, pokemon, setInput, setPokemons, search, id}) {
-    function Suggestion(value, callback) {
-        setInput(value)
-        setPokemons("")
-    }
-  
-    return <p key={id} onClick={() => Suggestion(pokemon, search)} className={className}>{pokemon}</p>
+import Styles from "./PokemonList.module.css";
+
+function PokemonList({
+  className,
+  pokemon,
+  setInput,
+  setPokemons,
+  SearchPokemon,
+  id,
+}) {
+  function Suggestion(event) {
+    setInput(pokemon);
+
+    setPokemons("");
+  }
+
+  return (
+    <div>
+      <button className={Styles.Button} key={id} onClick={Suggestion}>
+        {pokemon}
+      </button>
+    </div>
+  );
 }
 
-export default PokemonList
+export default PokemonList;
